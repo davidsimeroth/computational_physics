24fae24f488b3172225e826ff21397b600939730
David Simeroth <davidsimeroth@users.noreply.github.com>
2015-11-06 16:25:22 -0500

    Update rad.cpp

--- rad.cpp
+++ rad.cpp
@@ -11,18 +11,16 @@
 #include <fstream>
 #include <string>
 #include <cmath>
-
 using namespace std;
-
-int main ( int argc, char *argv[] ) 
-{
+//main program
+int main (int argc, char *argv[]){
  // constants:
  const double pi {3.1415926}; //because i don't like typing this out
  bool tb {false}, qb {false}; //until i know otherwise, i don't this info
  int row=0, i, j, tt, qq, xx, yy, zz, nn; //row,node,cell, and qvar index
- double az, el, eps; //azimuth, elevation, and emissivity
+ double az, el, caz, cel, eps, beta; //azimuth, elevation, emissivity, and view
  struct ray {float x, y, z;} n, c, p, q; //view, cell, normals and p, q
- float tave; //cell ave temp
+ float a, tave; //cell area, ave temp
  float * xn, yn, zn, tn, qn; //arrays for x, y, z, t, and q
  string line; //for incoming lines
 
@@ -34,24 +32,17 @@ int main ( int argc, char *argv[] )
   if (!dat.is_open()) cout << 'Error opening file!' << endl;
   else 
   {
-   // inputs:
-   cout << 'Azimuth of observer (degrees):' << endl;
-   cout << '0 = nose, 90 = right wing, -90 = left wing' << endl;
-   cin >> az;
-   az=az*pi/180;
-   cout << endl << 'Elevation of observer (degrees):' << endl;
-   cout << '0 = nose, 90 = top-down, -90 = bottom-up' << endl;
-   cin >> el;
-   el=el*pi/180;
-   while (getline (dat,line))
+   while (getline (dat,line)) 
    {
-    if (row==0) row=row+1;
-    if (row==1)
+    ++row
+    if (row==1);
+    else if (row==2)
     {
-     nn = count(line)-1;
-     
+     line = line + " "
+     nn = count(line) - 2;
+     xx = find
+    }
 	}
-   }
    dat.close();
    //qn=input('number of variables in output'); % TO BE READ FROM HEADER
    //i =input('number of points in output');    % ""
@@ -64,12 +55,20 @@ int main ( int argc, char *argv[] )
    cout << endl << 'Mirrored? (0=no,1=yes)' << endl; // if mirrored, extra logic
    cin >> mirror;
    // hopefully at some point, this will be automated.
+   // inputs:
+   cout << 'Azimuth of observer (degrees):' << endl;
+   cout << '0 = nose, 90 = right wing, -90 = left wing' << endl;
+   cin >> az;
+   az = az * pi / 180;
+   cout << endl << 'Elevation of observer (degrees):' << endl;
+   cout << '0 = nose, 90 = top-down, -90 = bottom-up' << endl;
+   cin >> el;
+   el = el * pi / 180;
    // calculations:
-   nz=sin(el); % spherical normals
-   ny=cos(el)*sin(az); 
-   nx=cos(el)*cos(az);
+   n.z = sin(el); // spherical normals
+   n.y = cos(el) * sin(az); 
+   n.x = cos(el) * cos(az);
    // READ IN STUFF
-   cells=resize(in(qn*i:end),4,[]); % Not sure if this is the right syntax
    // I want to read everything after the last variable; this contains
    // the information about the connectivity of the cells.
    x=in(1:i); // If all data is read in a single column; x-coord of nodes
@@ -79,53 +78,43 @@ int main ( int argc, char *argv[] )
    // open these arrays
    for k=1:j
     // area nonsense
-    p(1)=x(cells(1,k))-x(cells(2,k));
-    p(2)=y(cells(1,k))-y(cells(2,k));
-    p(3)=z(cells(1,k))-z(cells(2,k));
-    q(1)=x(cells(1,k))-x(cells(3,k));
-    q(2)=y(cells(1,k))-y(cells(3,k));
-    q(3)=z(cells(1,k))-z(cells(3,k));
-    cx=p(2)*q(3)-p(3)*q(2); % x normal of face
-    cy=p(3)*q(1)-p(1)*q(3); % y normal of face
-    cz=p(1)*q(2)-p(2)*q(1); % z normal of face
-    a=sqrt(cx^2+cy^2+cz^2); % area of face
-    cx=cx/a; % normalized norms
-    cy=cy/a;
-    cz=cz/a;
-    cel=asin(cz); % cell elevation
-    caz=asin(cy/cos(cel); % cell azimuth
-    beta=acos(abs(nx*cx+ny*cy+nz+cz));
-    if (abs(cel-el)>=pi/2)
-     rad(k)=0;
-    elseif (abs(caz-az)>=90)
+    p.x = x(cells(1,k)) - x(cells(2,k));
+    p.y = y(cells(1,k)) - y(cells(2,k));
+    p.z = z(cells(1,k))-z(cells(2,k));
+    q.x = x(cells(1,k))-x(cells(3,k));
+    q.y = y(cells(1,k))-y(cells(3,k));
+    q.z = z(cells(1,k))-z(cells(3,k));
+    c.x = p.y * q.z - p.z * q.y; //x normal of face
+    c.y = p.z * q.x - p.x * q.z; //y normal of face
+    c.z = p.x * q.y - p.y * q.x; //z normal of face
+    a = sqrt(c.x * c.x + c.y * c.y + c.z * c.z); //area of face
+    c = c / a; //normalized norms
+    cel = asin(cz); //cell elevation
+    caz = asin(cy / cos(cel); //cell azimuth
+    beta = acos(fabs(n.x * c.x + n.y * c.y + n.z * c.z));
+    if (fabs(cel - el) >= pi / 2. || abs(caz - az) >= pi / 2. )
      rad(k)=0;
     else
-     %average t
      t=(tray(cells(1,k))+tray(cells(2,k)+tray(cells(3,k)))/3;
      qq=eps*sig*t^4;
      rad(k)=cos(beta)*a*qq;
-    end
-   end
    radtot=sum(rad);
    // fin
   }
  }
  return 0;
 }
-// rad subroutine
-float rad(float t, double eps)
-{
+//rad subroutine for blackbody emission
+float rad(float t, double eps){
  const float sig {5.670367e-8}; //W/m^2/K^4
  float q;
- q = eps*sig*pow(t,4e0);
+ q = eps * sig * pow(t,4);
  return q;
 }
-// simple program for counting inputs based off of spaces
-int count( const string input, int n{0} )
-{
- int nspace = 0;
- for (int i = n; i < input.size(); ++i)
- {
+//simple subroutine for counting spaces
+int count(const string input, int n{0}){
+ int nspace {0};
+ for (int i = n; i < input.size(); ++i){
   if (input[i] == ' ') ++nspace;
  }
  return nspace;
